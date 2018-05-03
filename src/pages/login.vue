49341e3f163eed8550191d7fca316cb8bb75f0a3
Your Name <316148334@qq.com>
2018-05-03 11:45:09 +0800

    登录1.0

---
+++ src/pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <yd-cell-group>
+            <yd-navbar title="登录"></yd-navbar>
+            <yd-cell-item>
+                <span slot="left">用户名：</span>
+                <yd-input slot="right" required v-model="input1" max="20" placeholder="请输入用户名"></yd-input>
+            </yd-cell-item>
+            <yd-cell-item>
+                <span slot="left">密码：</span>
+                <yd-input slot="right" type="password" v-model="input2" placeholder="请输入密码"></yd-input>
+            </yd-cell-item>
+       </yd-cell-group>
+       <yd-button size="large" type="primary">primary</yd-button>
+    </div>
+</template>
+// 登录
+<script type="text/babel">
+    export default {
+        name: "Login",
+        data() {
+            return {
+                input1: '',
+                input2: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
