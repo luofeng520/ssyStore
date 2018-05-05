81c4de09e8dd7b0f5712fb9ed8c859e134dfb434
Your Name <316148334@qq.com>
2018-05-05 17:51:20 +0800

    密码登录

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <yd-cell-group>
-            <yd-navbar title="登录"></yd-navbar>
+           <!--  <baseHeader :rightObj='rightObj'/> -->
             <yd-cell-item>
                 <span slot="left">用户名：</span>
                 <yd-input slot="right" required v-model="input1" max="20" placeholder="请输入用户名"></yd-input>
@@ -11,23 +11,46 @@
                 <yd-input slot="right" type="password" v-model="input2" placeholder="请输入密码"></yd-input>
             </yd-cell-item>
        </yd-cell-group>
-       <yd-button size="large" type="primary">primary</yd-button>
+       <p class='ver'>
+           <router-link to="">验证码登录</router-link>
+       </p>
+       <yd-button size="large" type="primary">登录</yd-button>
+       <p class="forgetpa">忘记密码</p>
     </div>
 </template>
-// 登录
 <script type="text/babel">
+// 登录
+    // import baseHeader from '@/components/header'
     export default {
         name: "Login",
         data() {
             return {
-                input1: '',
-                input2: ''
+                input1: '', 
+                input2: '',
+                rightObj:{
+                    show:true,
+                    rightTo:"/Rep",
+                    rightText:'注册'
+                }
             }
-        }
+        },
+        // components:{
+        //     baseHeader
+        // }
     }
 </script>
 
 <style scoped>
+    .forgetpa{
+        position:absolute;
+        text-align:center;
+        bottom:30px;
+        width:100%;
 
+    }
+    .ver{
+        text-align:right;
+        margin-right: 0.3em;
+    }
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/login'
-
 Vue.use(Router)
 
 export default new Router({
